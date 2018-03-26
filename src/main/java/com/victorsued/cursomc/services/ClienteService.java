0a6044b1e2492627f93a08a8c821a87bd11794d4
Victor Sued <visued@gmail.com>
2018-03-26 20:27:33 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/victorsued/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.victorsued.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victorsued.cursomc.domain.Cliente;
+import com.victorsued.cursomc.repositories.ClienteRepository;
+import com.victorsued.cursomc.services.expections.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+			
+		}
+		return obj;
+		
+	}
+
+}
