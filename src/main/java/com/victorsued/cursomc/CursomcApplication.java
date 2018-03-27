35314c023923ac20a4e54334347b970213c625b7
Victor Sued <visued@gmail.com>
2018-03-27 15:48:57 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/victorsued/cursomc/CursomcApplication.java
+++ src/main/java/com/victorsued/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.victorsued.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.victorsued.cursomc.domain.Cidade;
 import com.victorsued.cursomc.domain.Cliente;
 import com.victorsued.cursomc.domain.Endereco;
 import com.victorsued.cursomc.domain.Estado;
+import com.victorsued.cursomc.domain.Pagamento;
+import com.victorsued.cursomc.domain.PagamentoComBoleto;
+import com.victorsued.cursomc.domain.PagamentoComCartao;
+import com.victorsued.cursomc.domain.Pedido;
 import com.victorsued.cursomc.domain.Produto;
+import com.victorsued.cursomc.domain.enums.EstadoPagamento;
 import com.victorsued.cursomc.domain.enums.TipoCliente;
 import com.victorsued.cursomc.repositories.CategoriaRepository;
 import com.victorsued.cursomc.repositories.CidadeRepository;
 import com.victorsued.cursomc.repositories.ClienteRepository;
 import com.victorsued.cursomc.repositories.EnderecoRepository;
 import com.victorsued.cursomc.repositories.EstadoRepository;
+import com.victorsued.cursomc.repositories.PagamentoRepository;
+import com.victorsued.cursomc.repositories.PedidoRepository;
 import com.victorsued.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 
 	public static void main(String[] args) {
@@ -83,6 +95,23 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new  Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new  Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
+		
 				
 		
 	}
--- src/main/java/com/victorsued/cursomc/domain/Cliente.java
+++ src/main/java/com/victorsued/cursomc/domain/Cliente.java
@@ -13,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.victorsued.cursomc.domain.enums.TipoCliente;
@@ -36,6 +37,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente () {
 		
@@ -106,6 +110,14 @@ public class Cliente implements Serializable{
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
