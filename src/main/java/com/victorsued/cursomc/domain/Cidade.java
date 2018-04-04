435c2bfefb0018ef1fc7e3a3a102322dab2b9437
Victor Sued <visued@gmail.com>
2018-04-04 17:16:21 -0300

    Atualização utilizando somente @JsonIgnore

--- src/main/java/com/victorsued/cursomc/domain/Cidade.java
+++ src/main/java/com/victorsued/cursomc/domain/Cidade.java
@@ -7,8 +7,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Cidade {
 	
@@ -17,7 +15,6 @@ public class Cidade {
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
